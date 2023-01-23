e1a77a8e6047adad4ab09a815e4e80371354b321
Yomna Jehad <youssefjehad@yahoo.com>
2023-01-23 20:07:49 +0200

    chatapp 23/1

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:chatapp/screens/Register.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:chatapp/screens/login.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(ChatApp());
 }
 
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -3,86 +3,147 @@ import 'package:chatapp/screens/Register.dart';
 import 'package:chatapp/widgets/CustomButton.dart';
 import 'package:chatapp/widgets/customtexfield.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
-class LoginPage extends StatelessWidget {
+import 'package:chatapp/helper/showsnackbar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class LoginPage extends StatefulWidget {
   LoginPage({super.key});
   static String id = 'LoginPage';
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool isloading = false;
+  String? Email;
+  String? Password;
+
+  GlobalKey<FormState> formkey = GlobalKey();
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: kPrimaryColor,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8),
-        child: Column(
-          children: [
-            Spacer(
-              flex: 1,
-            ),
-            Image.asset('assets/images/scholar.png'),
-            Text('Scholar Chat',
-                style: TextStyle(
-                    fontSize: 32,
-                    color: Colors.white,
-                    fontFamily: 'Pacifico-Regular')),
-            Spacer(
-              flex: 1,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
+    return ModalProgressHUD(
+      inAsyncCall: isloading,
+      child: Scaffold(
+        backgroundColor: kPrimaryColor,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          child: Form(
+            key: formkey,
+            child: ListView(
               children: [
-                Text(
-                  'LOGIN',
-                  style: TextStyle(
-                    fontSize: 32,
-                    color: Colors.white,
-                  ),
+                SizedBox(
+                  height: 75,
+                ),
+                Image.asset(
+                  'assets/images/scholar.png',
+                  height: 100,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Scholar Chat',
+                        style: TextStyle(
+                            fontSize: 32,
+                            color: Colors.white,
+                            fontFamily: 'Pacifico-Regular')),
+                  ],
+                ),
+                SizedBox(
+                  height: 75,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      'LOGIN',
+                      style: TextStyle(
+                        fontSize: 32,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                CustomTextFormField(
+                  onChanged: (data) {
+                    Email = data;
+                  },
+                  hintText: 'Email',
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                CustomTextFormField(
+                  onChanged: (data) {
+                    Password = data;
+                  },
+                  hintText: 'Password',
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                CustomButton(
+                    onTap: () async {
+                      if (formkey.currentState!.validate()) {
+                        isloading = true;
+                        setState(() {});
+                        try {
+                          await RegisiterUser();
+
+                          ShowBar(context, 'Success');
+                        } on FirebaseAuthException catch (e) {
+                          if (e.code == 'weak-password') {
+                            ShowBar(context, 'Weak Password');
+                          } else if (e.code == 'email-already-in-use') {
+                            ShowBar(context, 'Email in used');
+                          }
+                        } catch (e) {
+                          ShowBar(context, 'there was an error');
+                        }
+                        isloading = false;
+                        setState(() {});
+                      } else {}
+                    },
+                    text: 'REGISTER'),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Dont have an account ? ',
+                        style: TextStyle(color: Colors.white)),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, RegisterPage.id);
+                      },
+                      child: Text(
+                        'Register',
+                        style: TextStyle(color: Color(0xffc7ede6)),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 80,
                 ),
               ],
             ),
-            SizedBox(
-              height: 10,
-            ),
-            CustomTextField(
-              hintText: 'Email',
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            CustomTextField(
-              hintText: 'Password',
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            CustomButton(text: 'LOGIN'),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Dont have an account ? ',
-                    style: TextStyle(color: Colors.white)),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.pushNamed(context, RegisterPage.id);
-                  },
-                  child: Text(
-                    'Register',
-                    style: TextStyle(color: Color(0xffc7ede6)),
-                  ),
-                )
-              ],
-            ),
-            Spacer(
-              flex: 3,
-            ),
-          ],
+          ),
         ),
       ),
     );
+  }
+
+  Future<void> RegisiterUser() async {
+    var auth = FirebaseAuth.instance;
+    UserCredential user = await auth.signInWithEmailAndPassword(
+        email: Email!, password: Password!);
   }
 }
